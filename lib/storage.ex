843be7b8748e58884e33823fa2e811f7b83040f0
Will Ferguson <william.ferguson@wipro.com>
2017-12-05 20:07:48 +0000

    Starting tests. Need to convert Pages %{} to [%{}]

--- lib/storage.ex
+++ lib/storage.ex
@@ -1,7 +1,7 @@
 defmodule Storage do
   use Agent
 
-  defstruct [visited: MapSet.new, to_visit: [], results: Map.new]
+  defstruct [visited: MapSet.new, to_visit: [], pages: %{}]
   @moduledoc """
 
   """
@@ -9,21 +9,24 @@ defmodule Storage do
   @doc """
     Starts our agent
   """
-  def start_link do
+  def start_link(_opts) do
     Agent.start_link(fn -> %Storage{} end)
   end
 
   @doc """
-      Submits the result of a crawl.
+      Submits the result of a crawl for a single page.
+      Page might look like:
+        %{:a => ["abc.com", "bbc.com"], :img => []}
   """
-  def submit(agent, results) do
+  def submit_page(agent, {page_name, page_data}) do
     fun = fn data ->
-      #stored_results = data.results
+      new_map = Map.put(data.pages, page_name, page_data)
+      %{data | pages: new_map}
     end
     Agent.update(agent, fun, 5000)
-
   end
 
-
-
+  def get_pages(agent) do
+    Agent.get(agent, fn data -> data.pages end, 5000)
+  end
 end
